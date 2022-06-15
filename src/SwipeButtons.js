615dafea6cd98a4195aa2d81b6a8a2353521a21b
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2022-06-15 18:40:01 -0400

    Update

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,9 +1,20 @@
 import "./SwipeButtons.css";
 import ReplayIcon from "@material-ui/icons/Replay";
 import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/icons/IconButton";
 
 function SwipeButton() {
-  return <div className="swipeButtons"> </div>;
+  return (
+    <div className="swipeButtons">
+      <IconButton>
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+
+      <IconButton>
+        <CloseIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
 }
 
 export default SwipeButton;
